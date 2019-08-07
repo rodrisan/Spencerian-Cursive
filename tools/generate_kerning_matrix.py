a3db0028569d0e9d425ca3b466855a6021d66362
Alireza Alipour <14098141+AlirezaAlipour@users.noreply.github.com>
2019-08-07 15:18:53 +0430

    I'm progressing in coding for genrating matrix for kerning.

--- tools/generate_kerning_matrix.py
+++ tools/generate_kerning_matrix.py
@@ -1,4 +1,5 @@
 import fontforge
+import numpy as np
 
 final_forms = {'a': ['a', 'd', 'h', 'i', 'k', 'l', 'm', 'n', 'p', 'r', 't', 'u', 'x'],
                'c': ['c', 'e'],
@@ -23,11 +24,54 @@ for glyph in my_type:
     elif len(ccmp_lookup_s):
             ccmp_lookup=ccmp_lookup_s
 
-    if len(ccmp_lookup) == 1:
+    if len(ccmp_lookup):
         # becaue it like (((x,x),(x,x)),)
         ccmp_lookup_refined=ccmp_lookup[0][2:]
         pairs=pairs | {(ccmp_lookup_refined[x], ccmp_lookup_refined[x+1])
                          for x in range(len(ccmp_lookup_refined)-1)}
 
+row_set={x[0] for x in pairs}
+cloumn_set={x[1] for x in pairs}
 
-print(len(pairs))
+#Convertin above sets to list to in order to support for indexing
+row=[x for x in row_set]
+cloumn=[x for x in cloumn_set]
+
+
+kerning_matrix=np.zeros((len(row),len(cloumn)))
+
+
+for pair in pairs:
+        left_part=pair[0]
+        right_part=pair[1]
+        
+        #index will start from 1 if exist a value
+        row_index=row.index(left_part)
+        cloumn_index=cloumn.index(right_part)
+        
+
+        right_part_anchor_x=0
+        left_part_anchor_x=0
+        
+        for anchor_class_name,anchor_type,anchor_x,anchor_y in my_type[left_part].anchorPoints:
+                if anchor_type == 'exit':
+                        left_part_anchor_x=anchor_x
+
+        for anchor_class_name,anchor_type,anchor_x,anchor_y in my_type[right_part].anchorPoints:
+                if anchor_type == 'entry':
+                        right_part_anchor_x=anchor_x
+
+        distance=((my_type[left_part].width-left_part_anchor_x)+right_part_anchor_x)*-1
+        kerning_matrix[row_index][cloumn_index]=distance
+
+
+distances=[]
+for x in kerning_matrix:
+        for y in x:
+                distances[len(distances):]=[y]
+my_type.addKerningClass("'kern' cursive feature",'test',row,cloumn,distances)
+
+#print(distances)
+
+my_type.save('../sources/SpencerianCursive_WithKerneringMatrix.sfd')
+
