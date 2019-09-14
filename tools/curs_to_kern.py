be2a4c7121047024ffd93ca5d59ef5016088f0b2
Alireza Alipour <14098141+AlirezaAlipour@users.noreply.github.com>
2019-09-14 19:37:21 +0430

    I'm progressing.

--- tools/curs_to_kern.py
+++ tools/curs_to_kern.py
@@ -3,104 +3,41 @@ from pathlib import Path
 import sys
 import importlib
 import numpy as np
+import copy
 sys.path.append('/usr/local/lib/python3.7/site-packages')
 # Import Fontforge
 # Becasue pylint give me error due to importing, I import it programmitically
 fontforge = importlib.import_module('fontforge')
 # --------------------------------------------------------------------------------------------------
-
-# Fontforge file(.sdf file) path
-sdf_file_path = '../sources/SpencerianCursive.sfd'
-
-# creating fontforge object
-fontforge_object = fontforge.open(sdf_file_path)
-
-# Check whether '.temp' exists or not
-if Path('.temp').exists() != True:
-    Path('.temp').mkdir()
-
-# Genereate a true type font
-fontforgeObject.generate('./.temp/font.ttf')
-
-# Opening the target font in the '.temp' directory
-type_face = TTFont('./.temp/font.ttf')
-
-# Retrive GSUB lookups from the GSUB table
-gsub_lookups = type_face['GSUB'].table.LookupList.Lookup
-
-# Retrive LookupTypes, including LookupType 1, LookupType 2 and LookupType 6
-lookupType1s, lookupType2s, lookupType6s = [], [], []
-
-for lookup in gsub_lookups:
-    if lookup.LookupType == 1:
-        lookupType1s.append(Lookup)
-    elif lookup.LookupType == 2:
-        lookupType2s.append(Lookup)
-    elif lookup.LookupType == 6:
-        lookupType6s.append(Lookup)
-
-# Finding possible pairs between two glyphs for example between a and b, not between parts of a or b,
-# so I need to iterate over LookupType6
-for lookup in lookupType6s:
-
-    # In this project each lookup has only one subtable, as I know it is limitation that we can't
-    #   have more than one subtable, for more information vist: https://fontforge.github.io/lookups.html
-    sub_table = Lookup.SubTable[0]
-
-    # For LookupType6 I have used only format 2 and 3
-    if sub_table.Format == 2:
-        # It is worth to mention that in Fontforge LookupType6 with Format2 has two ways to editing
-        #   one way in simple way and another in complex way, but I have only use the simple way,
-        #   in other word I have edited the Matche Classes list, so I need only use the InputClassDef
-        #   nor the BacktrackClassDef and the LookAheadClssDef
-
-        # The class list is like the Matche Classes list in FontForge *
-        classes = [[]] * max(sub_table.InputClassDef.classDefs.values())
-
-        # Generate Classes
-        for v, k in sub_table.InputClassDef.classDefs.items():
-            temp_list = Classes[k-1][:]
-            temp_list.append(v)
-            classes[k-1] = temp_list
-
-        # Apply Classes
-        for chain_sub_class in sub_table.ChainSubClassSet:
-            #
-            if chain_sub_class != None:
-                #
-                for subst_lookup_record in chain_sub_class.ChainSubClassRule[0].SubstLookupRecord:
-                    print('ok')
-                    # Index of current ChainSubClass in SubTable.ChainSubClassSet, currenct index is index of
-                    #   the class mines one that I need appy substituation of it
-                    ChainSubClassIndex = ""
-                    #
-
-    elif SubTable.Format == 3:
-        print('format3')
-
-
 # The following method will apply the substituions on strings of the glyphs
 def apply_substitution(l_l_g, substitutions):
     """
-        l_l_g: It is list of list of glyphs, it is worth to mention that l_l_g
-        must be a numpy matrix.
+        l_l_g: It is list of list of glyphs
 
-        substitutions: It is list of ['substituation_index','position to apply'],
+        substitutions: It is list of ['substituation_dictionaries','position to apply'],
         it is worth to mention that position to apply is posiition the list
         encomposing glyphs that the substitution will be apply on each glyph.  
     """
-
+    #
+    l_l_g=copy.deepcopy(l_l_g)
     # The follow loop will itereate over each substitution
-    for substitution in substitutions:
+    for substitution_dictionary,position in substitutions:
         # Retrive substitution dictionary !
-        substitution_dictionary = substitution
-        # Apply the substitution on the related position
-        for index, glyphs in enumerate(l_l_g):
-            for glyph in glyphs:
-                if glyph in substitution_dictionary:
+                # Apply the substitution on the related position
+        for base, replacement in substitution_dictionary.items():
+            for glyph in l_l_g[position]:
+                if base==glyph:
+                    l_l_g[position][l_l_g[position].index(glyph)] =replacement
 
-                    l_l_g[index][glyph] = substitution_dictionary[glyph]
+                    
 
+        # for index, glyphs in enumerate(l_l_g):
+        #     for glyph in glyphs:
+        #         if glyph in substitution_dictionary:
+
+        #             l_l_g[index][glyph] = substitution_dictionary[glyph]
+
+    #return l_l_g.toList()
     return l_l_g
 
 
@@ -113,26 +50,37 @@ def generate_possible_string(l_l_g):
     for glyphs in l_l_g:
         matrix_width = matrix_width*len(glyphs)
 
-    possible_string_matrix = np.zeros((matrix_width, matrix_height))
+    possible_string_matrix =[[]*matrix_width]*matrix_height
+    #  possible_string_matrix = np.empty((matrix_height,matrix_width),dtype='<U10')
 
     # Initialize above matrix
     for index, glyphs in enumerate(l_l_g):
         # the glyphs is a list of glyphs
-        possible_string_matrix[index] = matrix_width/len(glyphs)*glyphs
+        possible_string_matrix[index] = int(matrix_width/len(glyphs))*glyphs
 
     # List of possible strins
     possible_strings = list()
 
+    temporary_list=np.array(possible_string_matrix) 
+    
     # Iterate over y
-    for column in np.nditer(possible_string_matrix, order='F', flags=['external_loop']):
+    for column in np.nditer(temporary_list, order='F', flags=['external_loop','refs_ok']):
         possible_strings.append(column)
 
     # Solving problem of ccmp lookup, because some elements in possible_strings list contains several glyphs
     #   I need to * the list of glyphs( or parts) that form a glyph, then connect it to *,
     #   for example, if I have a list like [x1,x2,x3,[x4,x5]], I will convert it to [x1,x2,x3,x4,x5]
     for index, p_s in enumerate(possible_strings):
-        possible_string = np.array(p_s)
-        possible_strings[index] = [x for x in possible_string.flat]
+        possible_string = list()
+        for x in p_s:
+            if type(x)==list:
+                for glyph in x:
+                    possible_string.append(glyph)
+            else:
+                possible_string.append(x)
+
+        possible_strings[index] = possible_string
+
 
     return possible_strings
 
@@ -145,6 +93,114 @@ def generate_pairs(possible_sttrings):
 
     return pairs
 
+#---------------------------------------------------------------------------------------------------
+# Fontforge file(.sdf file) path
+sdf_file_path = '../sources/SpencerianCursive.sfd'
+
+# creating fontforge object
+fontforge_object = fontforge.open(sdf_file_path)
+
+# Check whether '.temp' exists or not
+if Path('.temp').exists() != True:
+    Path('.temp').mkdir()
+
+# Genereate a true type font
+fontforge_object.generate('./.temp/font.ttf')
+
+# Opening the target font in the '.temp' directory
+type_face = TTFont('./.temp/font.ttf')
+
+# Retrive GSUB lookups from the GSUB table
+gsub_lookups = type_face['GSUB'].table.LookupList.Lookup
+
+# Retrive LookupTypes, including LookupType 1, LookupType 2 and LookupType 6
+lookupType1s, lookupType2s, lookupType6s = [], [], []
+
+# Pairs
+all_pairs=set()
+
+for lookup in gsub_lookups:
+    if lookup.LookupType == 1:
+        lookupType1s.append(lookup)
+    elif lookup.LookupType == 2:
+        lookupType2s.append(lookup)
+    elif lookup.LookupType == 6:
+        lookupType6s.append(lookup)
+
+# Finding possible pairs between two glyphs for example between a and b, not between parts of a or b,
+# so I need to iterate over LookupType6
+for lookup in lookupType6s:
+
+    # In this project each lookup has only one subtable, as I know it is limitation that we can't
+    #   have more than one subtable, for more information vist: https://fontforge.github.io/lookups.html
+    sub_table = lookup.SubTable[0]
+
+    # For LookupType6 I have used only format 2 and 3
+    if sub_table.Format == 2:
+        # It is worth to mention that in Fontforge LookupType6 with Format2 has two ways to editing
+        #   one way in simple way and another in complex way, but I have only use the simple way,
+        #   in other word I have edited the Matche Classes list, so I need only use the InputClassDef
+        #   nor the BacktrackClassDef and the LookAheadClssDef
+
+        # The class list is like the Matche Classes list in FontForge *
+        classes = [[]] * (max(sub_table.InputClassDef.classDefs.values())+1)
+
+        # Generate Classes
+        for v, k in sub_table.InputClassDef.classDefs.items():
+            temp_list = classes[k][:]
+            temp_list.append(v)
+            classes[k] = temp_list
+
+        # Apply Classes
+        for index, chain_sub_class in enumerate(sub_table.ChainSubClassSet):
+            #
+            if chain_sub_class != None:
+                l_l_g=list()
+
+                l_l_g[len(l_l_g):]=[classes[x] for x in chain_sub_class.ChainSubClassRule[0].Backtrack]
+                l_l_g.append(classes[index])
+                l_l_g[len(l_l_g):]=[classes[x] for x in chain_sub_class.ChainSubClassRule[0].Input]
+                l_l_g[len(l_l_g):]=[classes[x] for x in chain_sub_class.ChainSubClassRule[0].LookAhead]
+
+                substitutions=[(gsub_lookups[s_l_r.LookupListIndex].SubTable[0].mapping ,s_l_r.SequenceIndex) 
+                for s_l_r in chain_sub_class.ChainSubClassRule[0].SubstLookupRecord]
+
+                a_l_l_g=apply_substitution(l_l_g,substitutions)
+                possible_strings=generate_possible_string(a_l_l_g)
+                all_pairs=all_pairs| generate_pairs(possible_strings)
+
+
+
+
+
+                #
+                for subst_lookup_record in chain_sub_class.ChainSubClassRule[0].SubstLookupRecord:
+                    print('ok')
+                    # Index of current ChainSubClass in SubTable.ChainSubClassSet, currenct index is index of
+                    #   the class mines one that I need appy substituation of it
+                    ChainSubClassIndex = ""
+                    #
+
+    elif sub_table.Format == 3:
+        
+        print('format3')
+
+        l_l_g=list()
+
+        l_l_g[len(l_l_g):]=[x.glyphs for x in sub_table.BacktrackCoverage]
+        l_l_g[len(l_l_g):]=[x.glyphs for x in sub_table.InputCoverage]
+        l_l_g[len(l_l_g):]=[x.glyphs for x in sub_table.LookAheadCoverage]
+
+        substitutions=[(gsub_lookups[s_l_r.LookupListIndex].SubTable[0].mapping ,s_l_r.SequenceIndex) 
+        for s_l_r in sub_table.SubstLookupRecord]
+
+        a_l_l_g=apply_substitution(l_l_g,substitutions)
+        possible_strings=generate_possible_string(a_l_l_g)
+        all_pairs=all_pairs| generate_pairs(possible_strings)
+
+
+
+print(all_pairs)
 
 
 # In order to know more about GSUB lookup types visit: https://docs.microsoft.com/en-us/typography/opentype/spec/gsub
