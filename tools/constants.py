34f1c4acc97b12a6e3059d5771d947083f54562c
Alireza Alipour <14098141+AlirezaAlipour@users.noreply.github.com>
2019-10-08 16:26:38 +0330

    I have writed an algorithm to find and save the outline distances, also I have added 'lowercasses' in 'constant.py'.

--- tools/constants.py
+++ tools/constants.py
@@ -2,6 +2,8 @@
 capitals = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
             'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
 
+lowercase = {x.lower() for x in capitals}
+
 # Type Constants
 
 em_size = 4096
--- tools/generate.py
+++ tools/generate.py
@@ -6,6 +6,8 @@ import numpy as np
 import copy
 import matplotlib.pyplot as plt
 import constants as c
+import json
+
 sys.path.append('/usr/local/lib/python3.7/site-packages')
 # Import Fontforge
 # Becasue pylint give me error due to importing, I import it programmitically
@@ -386,90 +388,153 @@ finals.extend(lowercases_final)
 
 
 
-uniqe_initials=[]
+uniqe_initials=set()
 for x in initials:
     if (x in uniqe_initials)==False:
-        uniqe_initials.append(x)
+        uniqe_initials.add(x)
 
-uniqe_finals=[]
+uniqe_finals=set()
 for x in finals:
     if (x in uniqe_finals)==False:
-        uniqe_finals.append(x)
+        uniqe_finals.add(x)
 
-# Exporting all 'PNG' images out of fontforge_object
-for glyph in fontforge_object:
-    fontforge_object[glyph].export('./.temp/png_glyhs/'+glyph+'.PNG',100,1)
- 
+# Outlines Distance Dictionary
+o_d_dic=dict()
+
+# Exporting all 'PNG' images out of fontforge_object and generating outline distances
+
+if Path('o_d_dic.json').exists()!=True:
+    with open('o_d_dic.json',mode='x') as f:
+
+        
+
+        for glyph in uniqe_initials|uniqe_finals:
+
+            if glyph[0:4]!='Guid' and (glyph in c.lowercase)!=True:
+                # Each element of row has three element, but the first of is my need
+                # I need to calculate distance from left and right of the outline of each glyph
+                # If some part of image don't includes the glyph I need set distance as the follow
+                image_path='./.temp/png_glyhs/'+glyph+'.PNG'
+                fontforge_object[glyph].export(image_path,c.em_size-1,1)
+                image_matraix=plt.imread(image_path)
+
+                left_distances=[100000]*c.em_size
+                right_distances=[100000]*c.em_size
+
+                # Each element of row has three element, but the first of is my need, indeed v[0]
+
+                if glyph in uniqe_finals and glyph in uniqe_initials:
+
+                    for index,row in enumerate(image_matraix):
+                        for k,v in enumerate(row[::-1]):
+                            if v[0] ==0:
+                                left_distances[index]=k+1
+                                break
+
+                        for k,v in enumerate(row):
+                            if v[0] ==0:
+                                right_distances[index]=k+1
+                                break
+
+                elif glyph in uniqe_finals:
+
+                    for index,row in enumerate(image_matraix):
+                        for k,v in enumerate(row[::-1]):
+                            if v[0] ==0:
+                                left_distances[index]=k+1
+                                break
+                    
+                    right_distances=None
+
+                elif glyph in uniqe_initials:
+                    for index,row in enumerate(image_matraix):
+
+                        for k,v in enumerate(row):
+                            if v[0] ==0:
+                                right_distances[index]=k+1
+                                break
+
+                    left_distances=None
+
+
+                o_d_dic[glyph]=[left_distances,right_distances]
+
+                json.dump(o_d_dic,f,sort_keys=True,indent=True)
+
+                print(glyph)
+   
+
+
 
 
 def claculate_kerning(left_glyph,right_glyph):
-    matrix_height=101
-    left_matrix=np.array([[int(x[0]) for x in y] for y in plt.imread('./.temp/png_glyhs/'+left_glyph+'.PNG')])
-    right_matrix=np.array([[int(x[0]) for x in y] for y in plt.imread('./.temp/png_glyhs/'+right_glyph+'.PNG')])
+    # matrix_height=c.em_size
+    # left_matrix=np.array([[int(x[0]) for x in y] for y in plt.imread('./.temp/png_glyhs/'+left_glyph+'.PNG')])
+    # right_matrix=np.array([[int(x[0]) for x in y] for y in plt.imread('./.temp/png_glyhs/'+right_glyph+'.PNG')])
 
-    left_distances=np.array([100000]*matrix_height)
-    right_distances=np.array([100000]*matrix_height)
+    # left_distances=np.array([100000]*matrix_height)
+    # right_distances=np.array([100000]*matrix_height)
     
-    for index in range(len(left_matrix)-1):
+    # for index in range(len(left_matrix)-1):
 
-        for k,v in enumerate(left_matrix[index][::-1]):
-            if v ==0:
-                left_distances[index]=k+1
-                break
+    #     for k,v in enumerate(left_matrix[index][::-1]):
+    #         if v ==0:
+    #             left_distances[index]=k+1
+    #             break
 
-        for k,v in enumerate(right_matrix[index]):
-            if v ==0:
-                right_distances[index]=k+1
-                break
-    # ----------------------------------------------------------------------------------
-    # Section: Harmonizing Distances
-    # Harmonizing distance between two glyph, I mean minimum distance between the outline of two glyphs
+    #     for k,v in enumerate(right_matrix[index]):
+    #         if v ==0:
+    #             right_distances[index]=k+1
+    #             break
+    # # ----------------------------------------------------------------------------------
+    # # Section: Harmonizing Distances
+    # # Harmonizing distance between two glyph, I mean minimum distance between the outline of two glyphs
 
-    # Destance between two glyphs
-    distance=150
+    # # Destance between two glyphs
+    # distance=150
 
   
 
 
     
-    # if left_glyph in capitals and right_glyph in capitals:
-    #     distance=300
-    if left_glyph == 'quotesingle' or left_glyph == 'quotedbl' or right_glyph == 'quotesingle' or right_glyph == 'quotedbl':
-        distance = 80
-    # if left_glyph == 'glyph90' or right_glyph == 'glyph90':
-    #     distance=200
-    if left_glyph == 'period' or left_glyph=='glyph98' or left_glyph=='exclam' or right_glyph == 'period' or right_glyph=='glyph98' or right_glyph=='exclam':
-        distance=60
-    if left_glyph == 'underscore' or left_glyph=='hyphen' or  right_glyph == 'underscore' or right_glyph=='hyphen':
-        distance=70
+    # # if left_glyph in capitals and right_glyph in capitals:
+    # #     distance=300
+    # if left_glyph == 'quotesingle' or left_glyph == 'quotedbl' or right_glyph == 'quotesingle' or right_glyph == 'quotedbl':
+    #     distance = 80
+    # # if left_glyph == 'glyph90' or right_glyph == 'glyph90':
+    # #     distance=200
+    # if left_glyph == 'period' or left_glyph=='glyph98' or left_glyph=='exclam' or right_glyph == 'period' or right_glyph=='glyph98' or right_glyph=='exclam':
+    #     distance=60
+    # if left_glyph == 'underscore' or left_glyph=='hyphen' or  right_glyph == 'underscore' or right_glyph=='hyphen':
+    #     distance=70
     
     
-    # ----------------------------------------------------------------------------------
-    # Section: Virtual Outline
-    # For some glyph I need to assum a vertical line for some glyphs for example for left side of 7
-    if right_glyph == 'seven':
-        right_distances= np.array([min(right_distances)]*matrix_height)
-    if right_glyph == 'six':
-        right_distances= np.array([min(left_distances)]*matrix_height)
+    # # ----------------------------------------------------------------------------------
+    # # Section: Virtual Outline
+    # # For some glyph I need to assum a vertical line for some glyphs for example for left side of 7
+    # if right_glyph == 'seven':
+    #     right_distances= np.array([min(right_distances)]*matrix_height)
+    # if right_glyph == 'six':
+    #     right_distances= np.array([min(left_distances)]*matrix_height)
     
 
-    # ----------------------------------------------------------------------------------
+    # # ----------------------------------------------------------------------------------
     
 
     
    
 
-    sum_result=left_distances+right_distances
-    kerning_result=0
-    min_result=min(sum_result)
-    if min_result<100000:
-        # It is worth to mention that height of the png imgages is equal the the number I have specified
-        #   in 'ontforge_object[glyph].export('./.temp/png_glyhs/'+glyph+'.PNG',100,1)' plus one
-        current_kerning=min_result*(c.em_size/101)
-        kerning_result=(current_kerning-distance)*-1
+    # sum_result=left_distances+right_distances
+    # kerning_result=0
+    # min_result=min(sum_result)
+    # if min_result<100000:
+    #     # It is worth to mention that height of the png imgages is equal the the number I have specified
+    #     #   in 'ontforge_object[glyph].export('./.temp/png_glyhs/'+glyph+'.PNG',100,1)' plus one
+    #     current_kerning=min_result*(c.em_size/101)
+    #     kerning_result=(current_kerning-distance)*-1
   
     
-    return  kerning_result
+    return  0
 
     
 
@@ -483,7 +548,7 @@ for final_k,final_v in enumerate(uniqe_finals) :
         
 
 fontforge_object.addKerningClass("'kern' *",
-                       "'kern' *1", uniqe_finals, uniqe_initials,[x for x in k_m.flat])
+                       "'kern' *1", uniqe_finals, uniqe_initials,[x for x in k_m.flatten()])
 
 
 
